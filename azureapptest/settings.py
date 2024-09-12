64edda4d77c362f1e258c52f0897d13c579b1305
sharathtirumalaraju <48446664+sharathtirumalaraju@users.noreply.github.com>
2024-09-12 14:05:04 -0500

    Update settings.py

--- azureapptest/settings.py
+++ azureapptest/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j!zalv(_1q_hh&l65oxqhz7092$=_2vb-=li02_#z3#vu2z))6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['storiestest']
 
 
 # Application definition
