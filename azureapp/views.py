4e8dae1287c6522f6bbe013d64aede4e7a54e7ff
sharath tirumalaraju <pratyushapusapati@sharath-2.lan>
2024-09-12 17:04:28 -0500

    changed

--- azureapp/views.py
+++ azureapp/views.py
@@ -1,5 +1,28 @@
-from django.http import HttpResponse
+from django.http import render
+
+from azure.storage.blob import BlobServiceClient
+from django.conf import settings
+
+def get_images_from_container(container_name):
+    """
+    Retrieves a list of image URLs from a specified Azure Blob Storage container.
+    """
+    blob_service_client = BlobServiceClient.from_connection_string(settings.AZURE_CONNECTION_STRING)
+    container_client = blob_service_client.get_container_client(container_name)
+
+    image_urls = []
+    try:
+        blob_list = container_client.list_blobs()
+        for blob in blob_list:
+            if blob.name.endswith(('.jpg', '.jpeg', '.png')):
+                image_url = f'https://{settings.AZURE_ACCOUNT_NAME}.blob.core.windows.net/{container_name}/{blob.name}'
+                image_urls.append(image_url)
+    except Exception:
+        image_urls = []
+    
+    return image_urls
 
 def home(request):
-    return HttpResponse("Hello, world!")
+    travel_images = get_images_from_container('travel')
+    return render(request, 'travel.html', {'travel_images': travel_images})
 
