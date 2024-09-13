d30ba11ebf92b8515af6c9a078b039dab04860b8
sharathtirumalaraju <48446664+sharathtirumalaraju@users.noreply.github.com>
2024-09-13 11:33:00 -0500

    Update settings_prod.py

--- azureapptest/settings_prod.py
+++ azureapptest/settings_prod.py
@@ -91,6 +91,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
 AZURE_ACCOUNT_NAME = os.getenv('AZURE_ACCOUNT_NAME')
 AZURE_CONNECTION_STRING = os.getenv('AZURE_CONNECTION_STRING')
 
+CSRF_TRUSTED_ORIGINS = ['https://storiestest.azurewebsites.net']
+
 # # Security settings for production
 SESSION_COOKIE_SECURE = True  # Cookies only over HTTPS
 CSRF_COOKIE_SECURE = True  # Cross-site request forgery protection over HTTPS
